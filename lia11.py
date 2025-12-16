de2d606bb6e3b88e10df6e0c0722f6c3cbdb64d7
Lianna-03 <alina2009moskova@gmail.com>
2025-12-16 14:16:48 +0300

    Update lia11.py

--- lia11.py
+++ lia11.py
@@ -6,8 +6,6 @@ def time_to_travel_around_planet(robot_speed, planet_diameter):
     time = circumference / robot_speed
     return time
 
-# Пример использования функции с данными для демонстрации:
-# Допустим, скорость робота равна 10 км/ч, а диаметр Земли примерно 12742 км.
 robot_speed = 10
 planet_diameter = 12742
 robot_name = "Igor_bot V.2.0"
@@ -20,6 +18,5 @@ if isinstance(calculation_1, str):
 else:
     print(f"Роботу {robot_name} необходимо {calculation_1:.2f} часов, чтобы объехать вокруг планеты {planet_name}.")
 
-# Пример с неверными входными данными для проверки:
 calculation_2 = time_to_travel_around_planet(-10, 1000)
 print(f"\nПроверка обработки ошибки: {calculation_2}")
